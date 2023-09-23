aa3bd8b113013b56089cd85906d700a2a34f58e6
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-09-22 21:34:29 -0400

    fixed routing issues, updated note_id variable name to match starter code id, completed code for posting new note, need to work on delete section next

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const express = require('express');
 const path = require('path');
 //https://www.npmjs.com/package/uuid
 const { v4: uuidv4 } = require('uuid');
-uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+// uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const fs = require('fs');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -18,23 +19,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+});
 
 // GET Route for notes page
-app.get('/notes', (req, res) =>
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+});
 
 // GET Route for retrieving all the notes
-app.get('/notes', (req, res) => {
-    console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'))
 });
 
 // POST Route for a new note
-app.post('/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
 
     const { title, text } = req.body;
@@ -43,10 +43,23 @@ app.post('/notes', (req, res) => {
         const newNote = {
             title,
             text,
-            note_id: uuid(),
+            id: uuidv4(),
         };
 
-        readAndAppend(newNote, './db/db.json');
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+            } else {
+                console.log(data);
+                console.log(req.body);
+              const parsedData = JSON.parse(data);
+              parsedData.push(newNote);
+              console.log(parsedData);
+              fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
+              err ? console.error(err) : res.json(parsedData)
+            );
+            }
+          });
         res.json(`Note added successfully 🚀`);
     } else {
         res.error('Error in adding note');
@@ -54,22 +67,22 @@ app.post('/notes', (req, res) => {
 });
 
 // DELETE Route for a specific tip
-app.delete('/notes/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
-    console.log(noteId);
-    readFromFile('./db/db.json')
-        .then((data) => JSON.parse(data))
-        .then((json) => {
-            // Make a new array of all notes except the one with the ID provided in the URL
-            const result = json.filter((note) => note.note_id !== noteId);
+// app.delete('/notes/:id', (req, res) => {
+//     const noteId = req.params.id;
+//     console.log(noteId);
+//     readFromFile('./db/db.json')
+//         .then((data) => JSON.parse(data))
+//         .then((json) => {
+//             // Make a new array of all notes except the one with the ID provided in the URL
+//             const result = json.filter((note) => note.id !== noteId);
 
-            // Save that array to the filesystem
-            readAndAppend(result, './db/db.json');
+//             // Save that array to the filesystem
+//             readAndAppend(result, './db/db.json');
 
-            // Respond to the DELETE request
-            res.json(`Item ${noteId} has been deleted 🗑️`);
-        });
-});
+//             // Respond to the DELETE request
+//             res.json(`Item ${noteId} has been deleted 🗑️`);
+//         });
+// });
 
 // Listener
 app.listen(PORT, () =>
