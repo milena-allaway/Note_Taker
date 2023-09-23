1785c7a8d71a51250a5f4eb401efd0f7c6b5457f
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-09-22 23:31:51 -0400

    finished delete code: changed route to api/notes/:id, added comments, commented out console logs used for testing, added console logs to notify of successful add or delete

--- server.js
+++ server.js
@@ -1,88 +1,96 @@
 // starter code https://github.com/coding-boot-camp/miniature-eureka
 // import all the required dependencies
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 //https://www.npmjs.com/package/uuid
 const { v4: uuidv4 } = require('uuid');
-// uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
-const fs = require('fs');
-
+// define the port
 const PORT = process.env.PORT || 3001;
-
+// create an instance of express
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Middleware for serving static files
+//Middleware for serving static files from the public folder
 app.use(express.static('public'));
 
-// GET Route for homepage
+// GET Route for homepage to display index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-// GET Route for notes page
+// GET Route for notes page to display notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-// GET Route for retrieving all the notes
+// GET Route for retrieving all the notes from db.json
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'))
 });
 
-// POST Route for a new note
+// POST Route for a new note to the db.json file
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-
+    // console.info(`${req.method} request received to add a note`);
+    // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
-
+    // run if statement to check if the request body exists
     if (req.body) {
         const newNote = {
             title,
             text,
             id: uuidv4(),
         };
-
+        // read the db.json file and parse the data to an array of objects
+        // add the new note to the array of note objects and write the updated array to the db.json file
+        // return the new note to the client
+        // reference 11-Express\01-Activities\22-Stu_Modular-Routing from class, used util.js code to write and read file
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
               console.error(err);
             } else {
-                console.log(data);
-                console.log(req.body);
+                // console.log(data);
+                // console.log(req.body);
               const parsedData = JSON.parse(data);
               parsedData.push(newNote);
-              console.log(parsedData);
+            //   console.log(parsedData);
               fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
               err ? console.error(err) : res.json(parsedData)
-            );
-            }
-          });
-        res.json(`Note added successfully 🚀`);
+            )};
+        });
+        console.log('Note successfully added!')
     } else {
         res.error('Error in adding note');
-    }
+    };
 });
 
-// DELETE Route for a specific tip
-// app.delete('/notes/:id', (req, res) => {
-//     const noteId = req.params.id;
-//     console.log(noteId);
-//     readFromFile('./db/db.json')
-//         .then((data) => JSON.parse(data))
-//         .then((json) => {
-//             // Make a new array of all notes except the one with the ID provided in the URL
-//             const result = json.filter((note) => note.id !== noteId);
+// DELETE Route for a specific note
+app.delete('/api/notes/:id', (req, res) => {
+    // console.info(`${req.method} request received to delete a note`);
+    const noteId = req.params.id;
 
-//             // Save that array to the filesystem
-//             readAndAppend(result, './db/db.json');
-
-//             // Respond to the DELETE request
-//             res.json(`Item ${noteId} has been deleted 🗑️`);
-//         });
-// });
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // Get all notes from the db.json file, parse the data
+            const parsedData = JSON.parse(data);
+            // Filter out the note with the id that matches the noteId we are trying to delete
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+            const result = parsedData.filter((note) => note.id !== noteId);
+            // console.log(result);
+            // Write the filtered notes back to the db.json file
+            fs.writeFile('./db/db.json', JSON.stringify(result, null, 4), (err) =>
+            err ? console.error(err) : res.json(result)
+            );
+            console.log('Note successfully deleted!')
+        };  
+        
+    });
+});
 
 // Listener
 app.listen(PORT, () =>
